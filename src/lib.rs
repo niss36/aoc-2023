ba38dcb4e78961c3dd519be09ec3dca63666bf15
Nissim Chekroun <nissim.chekroun@gmail.com>
2023-12-01 18:04:32 +0000

    chore: initial setup

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+pub fn read_lines(path: &str) -> io::Result<Vec<String>> {
+    let path = Path::new(path);
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    reader.lines().collect()
+}
+
+pub fn to_lines(data: &str) -> Vec<String> {
+    data.lines().map(|s| s.to_owned()).collect()
+}
